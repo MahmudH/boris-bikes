f5708b10e342454e0e2ae08ddcb6b4da89a7ae50
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-10-13 17:19:38 +0100

    feature & unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,6 @@ require 'docking_station.rb'
 describe DockingStation do
 
 it  {expect(subject.respond_to? "release_bike").to be_truthy}
+it {expect((subject.release_bike).working?).to be true}
 
 end
