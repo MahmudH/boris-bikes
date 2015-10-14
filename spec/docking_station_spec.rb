8c3143e89d722bdb14ccd041a3b5de8a483d18c1
mahmudh <mahmudhssn246@gmail.com>
2015-10-14 17:18:49 +0100

    exercise 14 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,41 +1,42 @@
 # spec/docking_station_spec.rb
-require 'docking_station.rb'
+require 'docking_station'
 describe DockingStation do
 
   it  {is_expected.to respond_to :release_bike}
 
   it 'gets a bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
-
-
+    #bike = Bike.new
+    subject.dock(Bike.new)
+    last_bike = subject.bikes.last
+    expect(subject.release_bike).to eq last_bike
   end
 
-it "raise an error when trying to take out a bike" do
-  expect{ subject.release_bike }.to raise_error "No bikes available"
-end
+  it "raise an error when trying to take out a bike" do
+
+    expect{ subject.release_bike }.to raise_error "No bikes available"
+  end
 
 
-it {is_expected.to respond_to(:release_bike) }
+  it {is_expected.to respond_to(:release_bike) }
 
-it "test if bike works" do
-  bike = Bike.new
-  expect(bike).to be_working
-end
-
-it {is_expected.to respond_to(:dock).with(1).argument }
-
-it "be able to dock a bike" do
-  bike = Bike.new
-  expect(subject.dock(bike)).to eq bike
-end
-it "raise an error when trying to dock a bike" do
+  it "test if bike works" do
     bike = Bike.new
-    subject.dock(bike)
+    expect(bike).to be_working
+  end
 
-  expect{ subject.dock(bike) }.to raise_error "No space to dock"
-end
+  it {is_expected.to respond_to(:dock).with(1).argument }
+
+  it "be able to dock a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "raise an error when trying to dock a bike" do
+      bike = Bike.new
+      20.times { subject.dock Bike.new }
+      # subject.dock(bike)
+      expect{ subject.dock(bike) }.to raise_error "No space to dock"
+  end
 
 
 end
