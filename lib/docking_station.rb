1f68576137376d530466663e6d4593e0a4bd1c36
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-10-14 17:42:18 +0100

    ex 15 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,24 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock bike
-    fail "No space to dock" if @bikes.count == 20
+    fail "No space to dock" if full?
     @bikes << bike
     # @bikes.last
   end
+
+  private
+
+    def full?
+      @bikes.count >= 20 ? true : false
+    end
+    def empty?
+      @bikes.empty? ? true : false
+    end
+
+
 end
