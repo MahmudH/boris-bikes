90e9878e8112a747172478970c0a891b66c1af43
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-10-14 10:33:08 +0100

    test link mahmud

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@
 require 'docking_station.rb'
 describe DockingStation do
 
-it  {expect(subject.respond_to? "release_bike").to be_truthy}
+it  {expect(subject.respond_to? "release_bike").to be truth}
 it {expect((subject.release_bike).working?).to be true}
 
 end
